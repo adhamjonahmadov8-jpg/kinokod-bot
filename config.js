714cc0eee4fc5e1a30afee285e9508c3a6e0504c
adhamjonahmadov8-jpg <adhamjonahmadov8@gmail.com>
2026-09-04 21:37:57 +0500

    Update REQUIRED_CHANNELS in config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
   // Homiy kanallar ro'yxati (Kanal username'i yoki ID'si)
   REQUIRED_CHANNELS: [
-    '@sizning_kanalingiz_1', // Masalan: @KinoKod_Official
-    '@sizning_kanalingiz_2'  // Keyinchalik 3-sida bo'lsa, vergul qo'yib qo'shasiz
+    '@t.me/adhamjon_kino_kod'  // Keyinchalik 3-sida bo'lsa, vergul qo'yib qo'shasiz
   ]
 };
